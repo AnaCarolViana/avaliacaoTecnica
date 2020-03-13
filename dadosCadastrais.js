238dcca9a1aa98514c392ebac1583d71e56e7587
Ana Carol Viana <54864592+AnaCarolViana@users.noreply.github.com>
2020-03-13 12:44:41 -0300

    "GET" para cadastro salvo com sucesso

--- dadosCadastrais.js
+++ dadosCadastrais.js
@@ -70,4 +70,20 @@ function novoCadastro () {
   form.attr("disabled", false)
 }
 
+function pegarTodos() {
+    var endereco = {
+        "url": "http://entrevista.starcorp.com.br/api/Pessoas/GetAll",
+        "method": "GET",
+        "timeout": 0,
+        "headers": {
+            "Chave": "F4BA6C0A-4E6A-4AD4-A9E0-918A0EF9E55F"
+        }
+    }
+    $.ajax(endereco).done(function(response) {
+        console.log(response)
+        
+        
+    })
+    
+}
 
